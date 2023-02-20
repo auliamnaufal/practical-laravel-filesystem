547a7c8a0ea366415507317741dd79f54a528c91
auliamnaufal <auliamnaufal@gmail.com>
2023-02-20 23:20:31 +0700

    Feature: add toggle button to set image as public or not

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,13 @@
                                placeholder="Type here...">
                     </div>
 
+                    <label class="w-full relative inline-flex items-start cursor-pointer mt-4">
+                        <input type="checkbox" name="is_public" class="sr-only peer">
+                        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        <span class="ml-3 text-sm font-medium text-gray-900">Set Public</span>
+                    </label>
+
+
                     <button type="submit" class="mt-8 px-8 py-2 bg-gray-600 text-white rounded-lg">Upload</button>
                 </form>
             </div>
