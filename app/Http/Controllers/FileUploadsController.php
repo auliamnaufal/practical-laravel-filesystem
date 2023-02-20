6b7a6ffea0ba4994a657d027f56aa667732f500c
auliamnaufal <auliamnaufal@gmail.com>
2023-02-20 22:45:58 +0700

    Feature: check if file exist and if exist then store file to current disk

--- app/Http/Controllers/FileUploadsController.php
+++ app/Http/Controllers/FileUploadsController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 class FileUploadsController extends Controller
 {
     public function store(Request $request) {
-        return $request->file('file');
+
+        if (!$request->hasFile('file')) {
+            return back()->with('error', 'File not found. Please retry');
+        }
+
+        $path = $request->file('file')->store('files');
+
+        return $path;
     }
 }
