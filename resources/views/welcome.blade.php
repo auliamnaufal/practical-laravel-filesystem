c5b32869fdd0da58a3ee081250cdd30b548777a7
auliamnaufal <auliamnaufal@gmail.com>
2023-02-20 22:36:55 +0700

    Feature: Add basic form with 1 input type file that submitted to uploads.store route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,20 @@
         <main>
 
             <div class="h-screen max-h-screen max-w-screen-3xl mx-auto p-8">
-                <div class="w-full h-full bg-gray-200 rounded-lg">
+                <div class="w-full h-full bg-gray-200 p-4 rounded-lg grid grid-cols-3 gap-4">
+
+                    <div class="h-full col-end-2 bg-gray-300 rounded-lg flex justify-center items-center">
+                        <form method="POST" action="{{ route('uploads.store') }}" class="flex justify-center items-center flex-col" enctype="multipart/form-data">
+                            @csrf
+                            <label>
+                                <span class="sr-only">Choose File</span>
+                                <input type="file" name="file" class="block w-fit text-sm text-gray-600 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-gray-50 file:text-gray-600 hover:file:bg-blue-100"/>
+                            </label>
+                            <button type="submit" class="mt-8 px-8 py-2 bg-gray-600 text-white rounded-lg">Upload</button>
+                        </form>
+                    </div>
+
+                    <div class="h-full col-start-2 col-span-2 rounded-lg"></div>
 
                 </div>
             </div>
