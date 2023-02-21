1027b9ee3ff60be7509f2859df2702bdaea84c26
auliamnaufal <auliamnaufal@gmail.com>
2023-02-21 10:48:19 +0700

    Refactor: change img src method to use asset instead of URL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
 
             <div class="h-full col-start-2 col-span-2 grid grid-cols-3 gap-4 rounded-lg overflow-auto">
                 @foreach($files as $file)
-                    <img src="{{ URL::to('/') . $file }}" alt="images" class="w-full h-80 rounded-lg object-cover">
+                    <img src="{{ asset($file) }}" alt="images" class="w-full h-80 rounded-lg object-cover">
                 @endforeach
             </div>
 
